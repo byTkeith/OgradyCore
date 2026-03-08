dd590bb43a8b2389d928c44f1498a14a75c709fd
Tendai Nyevedzanai <keithsolo.sav@gmail.com>
2026-03-08 21:26:01 +0200

    automated ngrok-2

--- constants.tsx
+++ constants.tsx
@@ -64,4 +64,4 @@ export const SCHEMA_MAP: Record<string, { description?: string, primaryKeys: str
 };
 
 export const MOCK_CHART_COLORS = ['#10b981', '#3b82f6', '#f59e0b', '#ef4444', '#8b5cf6', '#ec4899', '#06b6d4'];
-export const DEFAULT_BRIDGE_URL = '';
+export const DEFAULT_BRIDGE_URL = 'https://unpanoplied-marianne-ciliately.ngrok-free.dev';
